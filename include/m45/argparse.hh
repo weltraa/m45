888553bf1d03acef73a9dd53dc19587cacc0ecfe
weltraa <113034629+weltraa@users.noreply.github.com>
2026-08-22 12:25:33 -0600

    first [] master: initial scaffolding template

---
+++ include/m45/argparse.hh
@@ -0,0 +1,10 @@
+/**
+ * M45 argparse
+ * Copyright (c) 2026 weltraa
+ * Licensed under the [LICENSE NAME] License.
+ */
+
+#ifndef M45_ARGPARSE_HH
+#define M45_ARGPARSE_HH
+
+#endif // m45/argparse.hh
---
+++ src/argparse.cc
@@ -0,0 +1,7 @@
+/**
+ * M45 argparse
+ * Copyright (c) 2026 weltraa
+ * Licensed under the [LICENSE NAME] License.
+ */
+
+#include <m45/argparse.hh>
